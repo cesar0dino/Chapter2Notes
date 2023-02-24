0845b0a0fbcab59247b539a2b8c7dccac7f5310e
de6y <de6y@fresnou.org>
2023-02-24 10:27:08 -0800

    forloops

--- forloopsReview.js
+++ forloopsReview.js
@@ -29,9 +29,41 @@ for(let i = 350; i<= 1000; i++) {
 
 //6
 let sum = 0;
-for(let i=1; i<=50; i++){
-    if(i % 2 == 0){
-        sum += i;
+for (let i = 0; i <= 50; i++){
+    sum += i;
+}
+console.log(sum);
+
+//7
+let sum = 0;
+for (let i = 35; i <= 70; i++){
+    sum += i;
+}
+console.log(sum);
+
+//8
+let sum = 1;
+for (let i = 1; i <= 15; i++){
+    sum *= i;
+}
+console.log(sum);
+
+//9
+let sum = 10;
+for (let i = 10; i <= 35; i++){
+    sum *= i;
+}
+console.log(sum);
+
+//10
+for(let i = 0; i<=12; i++){
+    console.log("Happy Holidays!");
+}
+
+//12
+for(let i = 100; i<= 200; i++){
+    if(i % 3 == 0){
+        console.log(i);
     }
 }
-console.log(`sum: ${sum}`);
+
